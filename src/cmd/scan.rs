3333b3cca3f691679fa83c5509a5d933f8cdec34
elhmn <elhmn@github.com>
2022-11-12 19:06:51 +0100

    Clone repository from url
    We are now able to clone and store a repository from a url.

--- src/cmd/scan.rs
+++ src/cmd/scan.rs
@@ -1,22 +1,26 @@
 use crate::config;
 use clap::Args;
+use git2::Repository;
 use std::fs;
 use std::path;
+use std::io::Error;
+use std::process;
+use url::{Host, Position, Url};
 
 #[derive(Args, Debug)]
 pub struct RunArgs {
     /// the path to the repository we want to scan
     #[clap(value_name = "REPOSITORY", index = 1)]
-    repos: Option<String>,
+    repository: Option<String>,
 }
 
 pub fn run(args: &RunArgs, conf: config::Config) {
-    let repos = args.repos.clone().unwrap_or("".to_string());
+    let repo = args.repository.clone().unwrap_or("".to_string());
 
-    fetch_repository(repos, conf);
+    clone_repository(repo, conf);
 }
 
-pub fn fetch_repository(repos: String, conf: config::Config) {
+pub fn clone_repository(repo: String, conf: config::Config) {
     //Create the temporary directory if it doesn't exist
     let path = path::Path::new(&conf.tmp_folder);
     if !path.exists() {
@@ -25,5 +29,59 @@ pub fn fetch_repository(repos: String, conf: config::Config) {
         });
         println!("`{}` Temporary folder was created", conf.tmp_folder);
     }
-    println!("I will fetch the repository than store it's content");
+
+    //Validate url and extract repository name
+    let repo_name: String;
+    let repo_owner: String;
+    let mut host_name: String;
+    let url = Url::parse(&repo);
+    match url {
+        Err(err) => {
+            println!("Failed to parse `{}` repository url: {}", repo, err);
+            return
+        },
+        Ok(url) => {
+            //Check that the repo is a url
+            if url.scheme() != "https" {
+               println!("Failed to fetch the repository: Repository not a https url");
+               return
+            }
+
+            //Extract repository name and owner
+            let path_segments: Vec<&str> = url.path().split("/").collect();
+            if path_segments.len() <= 2 {
+                println!("Failed to parse repository owner and name from `{}`", url);
+                return
+            }
+
+            repo_owner = path_segments[1].to_string();
+            repo_name = path_segments[2].to_string();
+            let host_str = url.host_str();
+            match host_str {
+                Some(h) => { host_name = format!("{}-", h) },
+                None => { host_name = "".to_string() }
+            }
+
+            host_name = host_name.replace(".", "-").to_string();
+        },
+    }
+
+    //Clone the repository if it doesn't exist on disk
+    let repo_path = format!("{}/{}{}-{}", conf.tmp_folder, host_name, repo_owner, repo_name);
+    let _git_repo: Repository;
+    let path = path::Path::new(&repo_path);
+    if !path.exists() {
+        _git_repo = match Repository::clone(repo.as_str(), &repo_path) {
+            Ok(git_repo) => git_repo,
+            Err(err) => {
+                println!("Failed to clone `{}` repository: {}", repo, err);
+                return;
+            },
+        };
+    } else {
+        println!("`{}` git repository already on disk", repo_path);
+        return
+    }
+
+    println!("`{}` repository cloned successfully", repo_path);
 }
