e0f7e2c45b6105bd41c934c55d59151d821c9f12
elhmn <elhmn@github.com>
2022-11-19 14:52:48 +0100

    set commit objects

--- src/extractor/git.rs
+++ src/extractor/git.rs
@@ -47,13 +47,12 @@ pub struct Tag {
     pub kind: ObjectKind,
     pub tagger: String,
     pub sha: String,
-    //object is the sha of the object the tagged is applied to
-    pub object: String,
+    //commit_sha is the sha of the commit object the tagged is applied to
+    pub commit_sha: String,
 }
 
 #[derive(Serialize, Default)]
 pub struct Commit {
-    pub name: String,
     //sha the git object hash
     pub sha: String,
     pub author: String,
@@ -86,6 +85,14 @@ pub struct Git {
     pub refs: HashMap<String, String>,
 }
 
+impl Object {
+    fn new() -> Object {
+        Object {
+            ..Default::default()
+        }
+    }
+}
+
 pub fn new(repo: &repo::Repo) -> Result<Git, String> {
     let git_data = match extrat_git_objects(&repo) {
         Ok(d) => d,
@@ -94,35 +101,53 @@ pub fn new(repo: &repo::Repo) -> Result<Git, String> {
        }
     };
 
-    println!("{}", serde_json::to_string(&git_data).unwrap()); // Debug
+    println!("{:}", serde_json::to_string(&git_data).unwrap()); // Debug
     Ok(git_data)
 }
 
 pub fn extrat_git_objects(repo: &repo::Repo) -> Result<Git, git2::Error> {
     let r = &repo.repo;
+    let mut walk = r.revwalk()?;
 
     //Get all repository references
-    let mut walk = r.revwalk()?;
     let mut oid: git2::Oid;
     for rf in repo.repo.references()? {
         //add each reference objects in the walker
         if let Some(ref_name) = rf.unwrap().name() {
             oid = r.revparse_single(ref_name)?.id();
-            println!("ref_name: [{}]", ref_name);
             walk.push(oid)?;
         }
     }
 
+    let mut objects: HashMap<String, Object> = HashMap::new();
+    //for each commit found from the references
     for w in walk {
-        println!("[{}]", w.unwrap().to_string())
+        let mut obj = Object::new();
+        let oid = w?.clone();
+        let commit = r.find_commit(oid)?;
+
+        obj.kind = ObjectKind::Commit;
+        obj.commit = Some(Commit{
+            author: commit.author().to_string(),
+            sha: commit.id().to_string(),
+            message: commit.message().unwrap_or("").to_string(),
+            tree: commit.tree()?.id().to_string(),
+            committer: commit.committer().to_string(),
+            parents: (|| -> Vec<String> {
+                let mut ids = vec![];
+                for id in commit.parent_ids() {
+                    ids.push(id.to_string());
+                }
+                ids
+            })(),
+        });
+
+        //add object in the objects HashMap
+        objects.insert(oid.to_string(), obj);
     }
 
     Ok(Git {
-        objects: HashMap::from([(
-            "sha".to_string(),
-            Object {
-                ..Default::default()
-            })]),
+        objects,
         ..Default::default()
     })
 }
