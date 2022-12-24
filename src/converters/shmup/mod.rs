a1538b1547985e1d3f76bc26a746039441bbf49b
elhmn <elhmn@github.com>
2022-12-24 12:42:48 +0100

    Add scenes
    Traverse the trees to build a list of scenes.
    The scenes object will later be populated with
    meaningful data.

--- src/converters/shmup/mod.rs
+++ src/converters/shmup/mod.rs
@@ -14,7 +14,7 @@ pub struct Data {
 #[derive(Serialize, Default)]
 pub struct Scene {
     pub entities: HashMap<String, Entity>,
-    pub sub_scenes: HashMap<String, Scene>,
+    pub sub_scenes: Vec<String>,
 }
 
 #[derive(Serialize, Default)]
@@ -41,14 +41,63 @@ pub fn new() -> ShmupConverter {
 }
 
 impl converters::Converter<Data> for ShmupConverter {
-    fn run(&self, _extracted_data: &extractor::Data) -> Result<Data, String> {
-        println!("ShmupConverter called!");
-        Ok(Data {
-            ..Default::default()
-        })
+    fn run(&self, extracted_data: &extractor::Data) -> Result<Data, String> {
+        Ok(build_converter_data(extracted_data))
     }
 
     fn name(&self) -> String {
         CONVERTER_NAME.to_string()
+    }
+}
+
+fn build_converter_data(extracted_data: &extractor::Data) -> Data {
+    let commit_oid = &extracted_data.git.ref_target.1;
+    let objs = &extracted_data.git.objects;
+
+    let mut data = Data {
+        ..Default::default()
+    };
+
+    //Get the initial commit
+    if let Some(commit) = &objs[commit_oid].commit {
+        let trees_oid = vec![commit.tree.clone()];
+        add_scenes(&trees_oid, &mut data, objs);
+    }
+
+    data
+}
+
+fn add_scenes(
+    trees_oid: &Vec<String>,
+    data: &mut Data,
+    objs: &HashMap<String, extractor::git::Object>,
+) {
+    for tree_oid in trees_oid {
+        if let Some(tree) = &objs[tree_oid].tree {
+            let mut scene = Scene {
+                ..Default::default()
+            };
+
+            // Create entities
+            for oid in &tree.objects {
+                if let Some(blob) = &objs[oid].blob {
+                    scene.entities.insert(oid.clone(), blob_to_entity(blob));
+                } else {
+                    scene.sub_scenes.push(oid.clone());
+                }
+            }
+
+            if !scene.sub_scenes.is_empty() {
+                add_scenes(&scene.sub_scenes, data, objs);
+            }
+
+            data.scenes.insert(tree.sha.clone(), scene);
+        }
+    }
+}
+
+fn blob_to_entity(_blob: &extractor::git::Blob) -> Entity {
+    Entity {
+        ..Default::default()
     }
 }
