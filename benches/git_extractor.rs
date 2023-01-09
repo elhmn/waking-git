d2468debf1cd803c8e939ced86fa2e9d7f83cf7b
elhmn <elhmn@github.com>
2023-01-09 13:42:54 +0100

    wip: preparation work for the parallel git extractor

--- benches/git_extractor.rs
+++ benches/git_extractor.rs
@@ -5,10 +5,12 @@ use lib::repo;
 use lib::utils::test;
 use std::time::Duration;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn bench_extractor(c: &mut Criterion) {
     test::setup();
-    // We can override the configuration on a per-group level
-    c.bench_function("git data extraction", |b| {
+
+    let mut group = c.benchmark_group("extractor");
+    // Measure the performance of the extract_git_objects function
+    group.bench_function("git data extraction", |b| {
         b.iter(|| {
             let conf = config::Config::new();
             let storage_folder = format!("{}/{}", conf.wake_path, "repos");
@@ -20,12 +22,27 @@ fn criterion_benchmark(c: &mut Criterion) {
             git::extract_git_objects(black_box(&r)).unwrap();
         })
     });
+
+    // Measure the performance of the par_extract_git_objects function
+    group.bench_function("git parallel data extraction", |b| {
+        b.iter(|| {
+            let conf = config::Config::new();
+            let storage_folder = format!("{}/{}", conf.wake_path, "repos");
+            let url = "https://github.com/osscameroon/osscameroon-website".to_string();
+            let r = match repo::new_repo_from_url(url, &storage_folder) {
+                Ok(r) => r,
+                Err(err) => panic!("{err:}"),
+            };
+            git::par_extract_git_objects(black_box(&r)).unwrap();
+        })
+    });
+
     test::teardown();
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(100).measurement_time(Duration::from_secs(500)).warm_up_time(Duration::from_secs(3));
-    targets = criterion_benchmark
+    targets = bench_extractor
 }
 criterion_main!(benches);
--- src/extractor/git.rs
+++ src/extractor/git.rs
@@ -121,6 +121,46 @@ pub fn new(repo: &repo::Repo) -> Result<Git, String> {
     Ok(git_data)
 }
 
+pub fn par_extract_git_objects(repo: &repo::Repo) -> Result<Git, git2::Error> {
+    let r = &repo.repo;
+
+    let mut ref_name = "refs/heads/master";
+    //Get default reference oid
+    //First we check for `master` and if `master` does not exist we fallback to `main`
+    let oid = match repo.repo.refname_to_id(ref_name) {
+        Ok(oid) => oid,
+        Err(_) => {
+            ref_name = "refs/heads/main";
+            repo.repo.refname_to_id(ref_name)?
+        }
+    };
+
+    //we create a first walker for the purpose of counting how many
+    //iteration we might need to perform.
+    //This is done because the git2::Revwalk object does not implement
+    //a Clone Trait and we struggled to use the method `walk.count()` on it
+    //neither &walk.count() nor walk.clone().count() allowed us to count
+    //and iterate over the revwalk iterator.
+    //
+    //I wish there was a better way to do it, but this is the solution
+    //I currently have
+    let mut walk = r.revwalk()?;
+    walk.push(oid)?;
+    let _count = walk.count();
+
+    //now we recreated the walker to perform the iteration
+    //we initially planned to use it for
+    walk = r.revwalk()?;
+    walk.push(oid)?;
+
+    let objects: HashMap<String, Object> = HashMap::new();
+    Ok(Git {
+        objects,
+        ref_target: (ref_name.to_string(), format!("{}", oid)),
+        ..Default::default()
+    })
+}
+
 pub fn extract_git_objects(repo: &repo::Repo) -> Result<Git, git2::Error> {
     let r = &repo.repo;
 
