d3df7db6d74fda49b81be13fa761e4ee51225ec9
elhmn <elhmn@github.com>
2023-02-04 21:43:36 +0100

    Return tarball

--- core/src/server/mod.rs
+++ core/src/server/mod.rs
@@ -1,6 +1,10 @@
+use crate::repo;
 use axum::{http::StatusCode, routing::get, Json, Router};
 use serde::Deserialize;
 use simple_logger;
+use std::fs::File;
+use flate2::Compression;
+use tar::Builder;
 
 pub fn run(port: String) {
     simple_logger::init().unwrap();
@@ -42,7 +46,7 @@ struct ScanRequest {
 async fn get_scan(Json(payload): Json<ScanRequest>) -> (StatusCode, String) {
     let repo = payload.repo_url.unwrap_or_default();
     let conf = crate::config::Config::new();
-    let (_, converted) = match scan(conf, repo) {
+    let (_, converted, repo) = match scan(conf, repo) {
         Ok(d) => d,
         Err(err) => {
             log::error!("Failed to scan data: {err}");
@@ -53,6 +57,21 @@ async fn get_scan(Json(payload): Json<ScanRequest>) -> (StatusCode, String) {
         }
     };
 
+    let file = File::create("tmp.tar").unwrap();
+    let mut a = Builder::new(file);
+
+    //     a.append_path(repo.extracted_file_path).unwrap();
+    a.append_file(
+        "converted.json".to_owned(),
+        &mut File::open(repo.converted_file_path.to_owned()).unwrap(),
+    )
+    .unwrap();
+    a.append_file(
+        "extracted.json".to_owned(),
+        &mut File::open(repo.extracted_file_path.to_owned()).unwrap(),
+    )
+    .unwrap();
+
     //TODO: send back tarball of scanned data
     //the tarball should contain 2 files, extracted.json and converted.json
     (StatusCode::OK, converted)
@@ -61,7 +80,7 @@ async fn get_scan(Json(payload): Json<ScanRequest>) -> (StatusCode, String) {
 async fn get_extracted(Json(payload): Json<ScanRequest>) -> (StatusCode, String) {
     let repo = payload.repo_url.unwrap_or_default();
     let conf = crate::config::Config::new();
-    let (extracted, _) = match scan(conf, repo) {
+    let (extracted, _, _) = match scan(conf, repo) {
         Ok(d) => d,
         Err(err) => {
             log::error!("Failed to extract data: {err}");
@@ -78,7 +97,7 @@ async fn get_extracted(Json(payload): Json<ScanRequest>) -> (StatusCode, String)
 async fn get_converted(Json(payload): Json<ScanRequest>) -> (StatusCode, String) {
     let repo = payload.repo_url.unwrap_or_default();
     let conf = crate::config::Config::new();
-    let (_, converted) = match scan(conf, repo) {
+    let (_, converted, _) = match scan(conf, repo) {
         Ok(d) => d,
         Err(err) => {
             log::error!("Failed to convert data: {err}");
@@ -96,7 +115,7 @@ async fn ping() -> &'static str {
     "pong"
 }
 
-fn scan(conf: crate::config::Config, repo: String) -> Result<(String, String), String> {
+fn scan(conf: crate::config::Config, repo: String) -> Result<(String, String, repo::Repo), String> {
     let mut git_repo = match crate::repo::clone_repository(&repo, &conf) {
         Ok(r) => r,
         Err(err) => {
@@ -121,5 +140,5 @@ fn scan(conf: crate::config::Config, repo: String) -> Result<(String, String), S
             }
         };
 
-    Ok((extracted_json_data, converted_json_data))
+    Ok((extracted_json_data, converted_json_data, git_repo))
 }
